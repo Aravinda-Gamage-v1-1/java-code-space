45c9fa140e88d50ce7b8421d2c3bcf55a83349de
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-09-17 00:16:20 +0530

    add java-02

---
+++ java-02/Main.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello ");
+        System.out.println("World");
+
+        System.out.println(10 + 20);
+        System.out.println(30 - 20);
+        System.out.println(30 * 20);
+        System.out.println(30 / 20);
+
+        // This is a single line comment
+        /* This is a
+         * multi line 
+         * comment
+         */
+    }
+}
