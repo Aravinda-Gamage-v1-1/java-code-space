7c32915734587ffd3a708eccf62a66c942364e4e
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-09-17 14:44:58 +0530

    Add java-03

---
+++ java-03/Main.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int age = 22;
+        System.out.println(age);
+
+        age = 26;
+        System.out.println(age);
+
+        final String name = "Aravinda"; // This variable can't overwrite.
+        System.out.println(name); 
+    }
+}
