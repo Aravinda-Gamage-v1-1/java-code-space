bb9248453c0a76d49e6ff72bcc8376ae4be3fcfb
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-09-17 21:20:26 +0530

    add java-04

---
+++ java-04-(data-type)/Main.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] args) {
+        byte byteNum = 2; // stores whole numbers from -128 to 127
+
+        short shortNum = 10; // stores whole numbers from -32,768 to 32,767
+
+        int intNum = 50; // stores whole numbers from -2,147,483,648 to 2,147,483,647
+
+        long longNum = 100; // stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+        float floatNum = 1.44f; // stores fractional numbers. sufficient for storing 6 - 7 decimals
+
+        double doubleNum = 3.44d; // stores fractional numbers. sufficient from storing 15 decimals
+
+        boolean booleanValues = true; // stores true or false values
+        
+        char charCharacter = 'A'; // stores a single character/letters or ASCII values
+    }
+}
