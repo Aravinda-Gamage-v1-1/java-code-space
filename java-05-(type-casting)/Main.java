b71bd07adb0be44a5a09cbff3af4c58f658ec92e
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-09-22 22:53:44 +0530

    update java-05

--- java-05-(type-casting)/Main.java
+++ java-05-(type-casting)/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-      int myInt = 9;
-      double myDouble = myInt; // Automatic casting: int to double
+      // Widening Casting
+      int wCInt = 9;
+      double wCDouble = wCInt; // Automatic casting: int to double
   
-      System.out.println(myInt);      // Outputs 9
-      System.out.println(myDouble);   // Outputs 9.0
+      System.out.println(wCInt);      // Outputs 9
+      System.out.println(wCDouble);   // Outputs 9.0
+
+      // Narrowing Casting
+      double myDouble = 9.78d;
+      int nCInt = (int) myDouble; // Manual casting: double to int
+  
+      System.out.println(myDouble);   // Outputs 9.78
+      System.out.println(nCInt);      // Outputs 9
     }
   }
