bf14cc7676849f5d611f6961743abda79df01619
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-09-26 23:27:43 +0530

    update java-06

--- java-06-(java-operator)/Main.java
+++ java-06-(java-operator)/Main.java
@@ -1,5 +1,9 @@
-package java-06-(java-operator);
-
-public class Main {
-    
+class Main {
+    public static void main(String[] args){
+        int x = 20 + 50;
+        int sum1 = 30 + 100;
+        int sum2 = sum1 + 300;
+        int sum3 = sum2 + sum1;
+        
+    }
 }
