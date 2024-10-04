40ea7a05605428dd0dc65c406953a8117b882f7e
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-10-04 23:34:08 +0530

    updata java-08

--- java-08-(if)/Main.java
+++ java-08-(if)/Main.java
@@ -6,5 +6,12 @@ class Main {
         if (x > y) {
             System.out.println("Good people");
         }
+
+        int time = 20;
+        if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }
     }
 }
