994ec47cbdcbe2eaef2ae975d5a9d8bc1baee898
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-10-08 23:30:16 +0530

    update java-10

---
+++ java-10-(while-loop)/Main.java
@@ -0,0 +1,17 @@
+class Main {
+        public static void main(String[] args){
+        int i = 0;
+        while (i < 5) { 
+            System.out.println("This is while loop " + i);
+            i++;
+        } // While loop
+
+        do {
+            System.out.println("This is do while loop " + i);
+            i++;
+        }
+        while (i < 10); // Do While loop
+
+        
+    }
+}
--- java-10-(while-loop)/README.md
+++ java-10-(while-loop)/README.md
@@ -4,3 +4,6 @@
 
 #### While loop
 * The while loop loops through a block of code as long as a specified condition is true.
+
+#### Do While loop
+* The do while loop is a variant of the while loop. This loop is execute the code block once, before checking if the condition is true, then it will repeat the loop as long as the condition is true.
