054c9ee731004e4200c92665be7bcae89a6c1a5f
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-10-10 21:35:15 +0530

    updata java-12

--- java-12-(Array)/Main.java
+++ java-12-(Array)/Main.java
@@ -1,7 +1,8 @@
 class Main {
     public static void main(String[] args) {
-        String[] car = {"Volvo", "Toyota", "Jeep", "Hummer"};
-        int[] num = {10, 30, 40, 50};
+        //Arrays
+        String[] car = { "Volvo", "Toyota", "Jeep", "Hummer" };
+        int[] num = { 10, 30, 40, 50 };
 
         System.out.println(car[1]);
         car[2] = "Opel";
@@ -12,8 +13,25 @@ class Main {
             System.out.println(car[i]);
         }
 
-        for (String i : car){
+        for (String i : car) {
             System.out.println(i);
+        }
+
+        //Multidimensional array
+        int[][] numbers = {{10,20,30},{40,50,60}};
+
+        System.out.println(numbers[1][0]); //output 40
+
+        for (int i = 0; i < numbers.length; ++i){
+            for (int j = 0; j < numbers[i].length; ++j){
+                System.out.println(numbers[i][j]);
+            }
+        }
+
+        for (int[] row : numbers){
+            for (int i : row){
+                System.out.println(i);
+            }
         }
     }
 }
--- java-12-(Array)/README.md
+++ java-12-(Array)/README.md
@@ -1,2 +1,10 @@
 ### Arrays
 * Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+
+` String[] cars = {"BMW", "Audi", "Volvo", "Hyundai"}; `
+
+#### Multi Dimensional Arrays
+* Multi dimensional array is an array of arrays.
+* They are useful when you want to store data as a tubular form, like a table with row and column.
+
+` int[][] numbers = {{1,2,3,4,5},{10,20,30,40,50}} `
