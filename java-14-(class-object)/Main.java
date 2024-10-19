251fe11b70ba81f96df940d2bfa0f138a682b8a5
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-10-19 14:56:36 +0530

    add java-14

---
+++ java-14-(class-object)/Main.java
@@ -0,0 +1,15 @@
+class Main {
+    int x = 5;
+    
+    public static void main(String[] args) {
+        Main myObj1 = new Main();
+        System.out.println(myObj1.x);
+
+        Main myObj2 = new Main();
+        System.out.println(myObj2.x);
+
+        Second myObjSecond = new Second();
+        myObjSecond.print();
+        System.out.println(myObjSecond.massage);
+    }
+}
---
+++ java-14-(class-object)/Second.java
@@ -0,0 +1,7 @@
+class Second {
+    String massage = "This is second class";
+
+    public void print(){
+        System.out.println(massage);
+    }
+}
