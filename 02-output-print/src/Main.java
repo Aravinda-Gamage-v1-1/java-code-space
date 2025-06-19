7b467f31c0597f88b82e8dce8c4430b14a84e39b
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2025-06-19 21:37:16 +0530

    learn java output methods

---
+++ 02-output-print/src/Main.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("I am learning Java.");
+        System.out.println("I am excited to write my first Java program.\n");
+        System.out.println("============================\n");
+        System.out.print("This is a print statement without a newline.");
+        System.out.print(" This is still the same line.\n\n");
+        System.out.println(3);
+        System.out.println(3 + 4);
+    }
+}
