ebc1242c009f615688bedb90839c287e3274c863
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2025-06-20 14:00:50 +0530

    add comments

---
+++ 03-comments/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is a single-line comment
+        System.out.println("Hello, World!"); // Print a greeting message
+
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         */
+    }
+}
